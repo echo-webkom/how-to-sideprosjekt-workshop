0e886c1709e8afb3d58c028d19913c8d80cd3c61
Kai Waløen <kai.waloen@gmail.com>
2024-03-20 18:11:06 +0100

    added inf101 video

---
+++ src/app/components/project-list.tsx
@@ -0,0 +1,7 @@
+export function ProjectList() {
+    return (
+        <div>
+            <h1>Here are the list of my projects that I have worked on</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
+    <div className = "text-center h-screen bg-gradient-to-tl from-green-900 to-green-400 text-black">
+    <h1 className = "text-2xl"> Hi, my name is Kai</h1>
+    <p>I think this is a quote that John Wayne said</p>
+    <h2>Anyways I am learning how to make a website</h2>
+    <h3 className = "text-3xl bg-gradient-to-tr from-cyan-950 to-green-800">What is it that I am doing here</h3> 
+    <ul>
+      <li>🚀 Next.js</li>
+      <li>🚀 Typescript</li>
+      <li>🚀 TailwindCSS</li>
+      <li>🚀 Vercel</li>
+    </ul>
     
-    <h1>Hello, it is me</h1>
+    <iframe className = "mx-auto" width="500" height="400" src="https://www.youtube.com/embed/456Mii_hQ90" title="BugShooter - a student game made in Java" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     
+    </div>
   );
 }
