d67834b3ecb654812c10c16ceb398bf6cfac9585
Kai Waløen <kai.waloen@gmail.com>
2024-03-20 17:01:10 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-full gap-10 p-24">
-      <h1 className="text-3xl font-medium">
-        Velkommen til {'"'}How to make a website{'"'}
-      </h1>
-
-      <p>
-        Om du vil ha oppgaver, les{" "}
-        <span className="bg-gray-200 font-mono py-1 px-2 rounded-md">
-          README.md
-        </span>
-        .
-      </p>
-    </main>
+    <div>
+    <h1>Hello</h1>>
+    </div>
   );
 }
