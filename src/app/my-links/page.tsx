7c5dfa229f883951b4c538d98e255571e21ad071
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 21:25:28 +0100

    Added the navigation bar

--- src/app/my-links/page.tsx
+++ src/app/my-links/page.tsx
@@ -1,11 +1,11 @@
 import {ProjectList} from "@/app/components/project-list";
-
+import {Navigation} from "@/app/components/navigation";
 export default function MyLinks() {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500">
             <div className="max-w-screen-sm mx-auto text-lg flex flex-col gap-6 py-24">
+                <Navigation />
                 <h2 className="text-2xl text-gray-100 mb-4">My links:</h2>
-                <ProjectList/>
             </div>
         </div>
     );
