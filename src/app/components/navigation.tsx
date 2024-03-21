86ea0c94cf861aad44fe40d40fefdc2602417d69
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 20:58:43 +0100

    Added navigation bar using file based routing

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -6,10 +6,10 @@ export function Navigation() {
             <Link href="/">
                 <a className="text-blue-400 hover:text-blue-200">Hjem</a>
             </Link>
-            <Link href="/my-links/page.tsx">
+            <Link href="@/app/my-links/page.tsx">
                 <a className="text-blue-400 hover:text-blue-200">Link to my HTB, THM, GitHub and LinkedIn</a>
             </Link>
-            <Link href="/my-projects/page.tsx">
+            <Link href="@/app/my-projects/page.tsx">
                 <a className="text-blue-400 hover:text-blue-200">My projects</a>
             </Link>
         </nav>
