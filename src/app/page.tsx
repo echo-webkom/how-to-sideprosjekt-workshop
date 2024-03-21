219ec47e2c5ef701ac971e4eb4ac2509e82b84f2
Kai.Waloen <kawal5842@uib.no>
2024-03-21 15:59:58 +0100

    change it back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,12 @@ export default function Home() {
   return (
     <div className="text-center h-screen bg-gradient-to-tl from-green-900 to-green-400 text-black">
       <div className="column-container">
-        <h1 className="text-left text-4xl font-bold text-white py-8">
-          <span className="text-cyan-400">K</span>
-          <span className="text-purple-400">W</span>
-          <span className="text-yellow-400">A</span>
-          <span className="text-red-400">L</span>
+        <h1 className="text-center text-4xl font-bold text-white py-8">
+          <span className="text-black">KWAL</span>
         </h1>
 
         <div className="text-column">
-         
+          <h2>I think this is a quote that John Wayne said</h2>
           <h2>Anyways I am learning how to make a website</h2>
           <h3 className="text-3xl bg-gradient-to-tr from-cyan-950 to-green-800">What is it that I am doing here</h3>
           <ul>
