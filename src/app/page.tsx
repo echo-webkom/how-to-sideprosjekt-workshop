a2057fd06158e4d361219546e1b1b834dcec7500
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 18:40:34 +0100

    Added call to new ProjectList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,24 @@
+import {ProjectList} from "@/app/components/project-list";
+
 export default function Home() {
   return (
-      <div>
-        <Menu />
+      <div className="from-inherit --tw-gradient-from: inherit var(--tw-gradient-from-position);">
+        <Header />
           <div className="text-center">
               <br/>
               <br/>
-                <Liste />
+                <ProjectList/>
                 <br/>
                 <p>Made by Trym, aka bomulls øye joe</p>
           </div>
       </div>
   );
 
-    function Menu() {
+    function Header() {
         return (
             <div>
                 <h1 className="text-4xl">Velkommen, hva vil du se?</h1>
             </div>
         );
     }
-
-    function Liste() {
-        return (
-            <ul>
-                <li>Kul</li>
-                <li>Snill</li>
-            </ul>
-        )
-    }
-
 }
