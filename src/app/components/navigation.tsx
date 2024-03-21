fd98d99f97b31538c39b4d9250d0333cd5564f4a
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 21:04:07 +0100

    Debugging

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export function Navigation() {
     return (
-        <nav className="flex gap-4">
+        <nav className="flex gap-4 text-center">
             <Link href="/">
-                <a className="text-blue-400 hover:text-blue-200">Hjem</a>
+                <a className="text-emerald-950 hover:text-blue-200">Hjem</a>
             </Link>
-            <Link href="@/app/my-links/page.tsx">
-                <a className="text-blue-400 hover:text-blue-200">Link to my HTB, THM, GitHub and LinkedIn</a>
+            <Link href="../../my-links/page.tsx">
+                <a className="text-emerald-950 hover:text-blue-200">Link to my HTB, THM, GitHub and LinkedIn</a>
             </Link>
-            <Link href="@/app/my-projects/page.tsx">
-                <a className="text-blue-400 hover:text-blue-200">My projects</a>
+            <Link href="../../../../my-projects/page.tsx">
+                <a className="text-emerald-950 hover:text-blue-200">My projects</a>
             </Link>
         </nav>
     );
