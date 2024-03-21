7b8ae303ae43fb1c163e0cb5c57a796bf7caca81
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 19:40:41 +0100

    Removed due to redundency

--- src/app/components/project-presentation.tsx
+++
@@ -1,9 +0,0 @@
-import {Project} from "next/dist/build/swc";
-
-interface ProjectPresentation {
-    projectName: string;
-}
-
-export function Present({projectName}: ProjectPresentation) {
-    return <h3 className="">Dette prosjektet heter {projectName}!</h3>
-}
