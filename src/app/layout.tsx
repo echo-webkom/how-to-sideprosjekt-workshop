ed1730de765717bac97b44de0f8192b41f9b806f
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 18:40:05 +0100

    Minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "How to sideprosjekt",
+  title: "Trym Jakobsen",
 };
 
 export default function RootLayout({
