760a554bc4d3440c33483ee19387d20be09533ee
Kai.Waloen <kawal5842@uib.no>
2024-03-21 15:53:04 +0100

    Shifted things to the left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className = "text-center h-screen bg-gradient-to-tl from-green-900 to-green-400 text-black">
-    <h1 className = "text-2xl"> Hi, my name is Kai</h1>
-    <p>I think this is a quote that John Wayne said</p>
-    <h2>Anyways I am learning how to make a website</h2>
-    <h3 className = "text-3xl bg-gradient-to-tr from-cyan-950 to-green-800">What is it that I am doing here</h3> 
-    <ul>
-      <li>🚀 Next.js</li>
-      <li>🚀 Typescript</li>
-      <li>🚀 TailwindCSS</li>
-      <li>🚀 Vercel</li>
-    </ul>
-      <Link href = "/" className = "text-blue-300 hover:text-blue-100"> Home
-      </Link>
+    <div className="text-center h-screen bg-gradient-to-tl from-green-900 to-green-400 text-black">
+      <div className="column-container">
+        <h1 className="text-left text-4xl font-bold text-white py-8">
+          <span className="text-cyan-400">K</span>
+          <span className="text-purple-400">W</span>
+          <span className="text-yellow-400">A</span>
+          <span className="text-red-400">L</span>
+        </h1>
 
-      <Link href = "/projects" className = "text-blue-300 hover:text-blue-100"> Projects
-      </Link>
+        <div className="text-column">
+          <h2>I think this is a quote that John Wayne said</h2>
+          <h2>Anyways I am learning how to make a website</h2>
+          <h3 className="text-3xl bg-gradient-to-tr from-cyan-950 to-green-800">What is it that I am doing here</h3>
+          <ul>
+            <li>🚀 Next.js</li>
+            <li>🚀 Typescript</li>
+            <li>🚀 TailwindCSS</li>
+            <li>🚀 Vercel</li>
+          </ul>
+        </div>
       
+
+      <div>
+        <Link href="/" className="text-blue-300 hover:text-blue-100"> Home </Link>
+        <Link href="/projects" className="text-blue-300 hover:text-blue-100"> Projects </Link>
+      </div>
+    </div>
     </div>
   );
 }
