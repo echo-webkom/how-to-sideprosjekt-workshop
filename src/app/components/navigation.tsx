1705aa7b57d14da2a934e7f27f689fa8991f98f8
Trym Jakobsen <trjak5231@uib.no>
2024-03-21 21:25:09 +0100

    Centered navigation box

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export function Navigation() {
     return (
-        <nav className="flex gap-4 text-center">
-            <Link href="/">
-                <a className="text-emerald-950 hover:text-blue-200">Hjem</a>
-            </Link>
-            <Link href="/my-links">
-                <a className="text-emerald-950 hover:text-blue-200">Link to my HTB, THM, GitHub and LinkedIn</a>
-            </Link>
-            <Link href="/my-projects">
-                <a className="text-emerald-950 hover:text-blue-200">My projects</a>
-            </Link>
-        </nav>
+        <div className="flex justify-center">
+            <nav className="flex gap-4 text-center">
+                <Link href="/">
+                    <a className="text-emerald-950 hover:text-blue-200">Hjem</a>
+                </Link>
+                <Link href="/my-links">
+                    <a className="text-emerald-950 hover:text-blue-200">Link to my HTB, THM, GitHub and LinkedIn</a>
+                </Link>
+                <Link href="/my-projects">
+                    <a className="text-emerald-950 hover:text-blue-200">My projects</a>
+                </Link>
+            </nav>
+        </div>
     );
 }
