51423ee948cde986e65f91a928ffb85796dec643
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-05-23 19:56:51 +0200

    Added some spacing and redesign.

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,19 +2,23 @@ import Link from "next/link";
 
 export function Navigation() {
   return (
-    <div className="flex justify-center">
+    <div className="pt-4 flex justify-center">
       <nav className="flex gap-4 text-center">
-        <Link href="/">
-          <a className="text-emerald-950 hover:text-blue-200">Hjem</a>
-        </Link>
-        <Link href="/my-links">
-          <a className="text-emerald-950 hover:text-blue-200">
-            Link to my HTB, THM, GitHub and LinkedIn
-          </a>
-        </Link>
-        <Link href="/my-projects">
-          <a className="text-emerald-950 hover:text-blue-200">My projects</a>
-        </Link>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <Link href="/">
+            <a className="text-emerald-950 hover:text-blue-200">Home</a>
+          </Link>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <Link href="/my-links">
+            <a className="text-emerald-950 hover:text-blue-200">Social links</a>
+          </Link>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <Link href="/my-projects">
+            <a className="text-emerald-950 hover:text-blue-200">My projects</a>
+          </Link>
+        </div>
       </nav>
     </div>
   );
