dd9212cca464de3ec8be25e0d8138a593a5414c0
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-06-04 09:23:29 +0200

    Fixed use of Link instead of putting an <a> inside of a link as text.

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -5,18 +5,23 @@ export function Navigation() {
     <div className="pt-4 flex justify-center">
       <nav className="flex gap-4 text-center">
         <div className="bg-white p-4 rounded-lg shadow-lg">
-          <Link href="/">
-            <a className="text-emerald-950 hover:text-blue-200">Home</a>
+          <Link href="/" className="text-emerald-950 hover:text-blue-200">
+            Home
           </Link>
         </div>
         <div className="bg-white p-4 rounded-lg shadow-lg">
-          <Link href="/my-links">
-            <a className="text-emerald-950 hover:text-blue-200">Social links</a>
+          <Link href="/my-links" className="text-emerald-950 hover:text-blue-200">
+            Social links
           </Link>
         </div>
         <div className="bg-white p-4 rounded-lg shadow-lg">
-          <Link href="/my-projects">
-            <a className="text-emerald-950 hover:text-blue-200">My projects</a>
+          <Link href="/my-projects" className="text-emerald-950 hover:text-blue-200">
+            My projects
+          </Link>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <Link href="/my-blog" className="text-emerald-950 hover:text-blue-200">
+            Blog posts
           </Link>
         </div>
       </nav>
