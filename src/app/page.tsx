28dc99cab0c0ce5c9a75ae9f0977ad5750a55e0a
Kai.Waloen <kawal5842@uib.no>
2024-06-17 12:44:12 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="text-center h-screen bg-gradient-to-tl from-green-900 to-green-400 text-black">
       <div className="column-container">
         <h1 className="text-center text-2xl font-bold text-white py-8">
-          <span className="text-black">Hi, my name is Kai</span>
+          <span className="text-black">Hi, my name is Kai 👋</span>
           
         </h1>
 
@@ -20,14 +20,16 @@ export default function Home() {
           
         <div className="text-column">
           
-          <h2>Stay tuned, this site is under construction!</h2>
+          <h4>Stay tuned, this site is under construction!
       
           In the meantime, scroll down to take a look at what I have worked on so far!
+          </h4>
+        
         </div>
       
 
       <div>
-            <h2>Here are the list of my projects that I have worked on</h2>
+            <h5>Here are the list of my projects that I have worked on</h5>
             
             <iframe className = "mx-auto" width="500" height="400" src="https://www.youtube.com/embed/456Mii_hQ90" title="BugShooter - a student game made in Java" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
             <p className = "text-center">Bugshooter - Semesteroppgave Inf101</p>
