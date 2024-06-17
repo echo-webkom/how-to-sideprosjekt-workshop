4189a9d4eaf54204c7dac465d26a17bf7b8056b5
Kai.Waloen <kawal5842@uib.no>
2024-06-17 14:32:47 +0200

    simplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,35 +19,10 @@ export default function Home() {
         <h3 className="text-xl font-bold py-8">
       
           Stay tuned, this site is under construction! <br />
-          In the meantime, scroll down to take a look at what I have worked on
-          so far!
       
           <br />
       
         </h3>
-      
-        <div className="video-container">
-
-          <div className = "iframe-wrapper">
-      
-          <iframe
-            className="responsive-iframe"
-            src="https://www.youtube.com/embed/456Mii_hQ90"
-            title="BugShooter - a student game made in Java"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          ></iframe>
-          </div>
-          <p className="video-caption">
-            BugShooter - Semesteroppgave Inf101 april 2023
-      
-          </p>
-        
-        <h4 className="text-xl font-bold py-8">
-          GitHub Repo for BugShooter is available here: eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-
-        </h4>
-      
-        </div>
       
       </div>
     
