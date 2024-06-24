f4993f59bb80729632c97d0abf5b61f2e64a2ff8
Kai.Waloen <kawal5842@uib.no>
2024-06-24 17:35:16 +0200

    rodent reboot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
        
         <h4 className="text-xl py-8">
       
-          I am proud to introduce my first original game, BugShooter ☝. This is a game written in Java using Swing in a period of 4 weeks as part of the Inf101 coursework. <br></br><br></br>
+          I am proud to introduce my first original game, BugShooter ☝! This is a game written in Java using Swing in a period of 4 weeks as part of the Inf101 coursework. <br></br><br></br>
           Growing up, I played a lot of Flash games on Newgrounds.com and my favorite were top down zombie shooters, so I challenged myself to make one. Making this game was a fun, educational, and stressful experience that took a lot of banging my head on the problem but things turned out quite nicely. One day, it occurred to me that instead of a zombie shooter, a bug shooter game would be more appropriate to reflect my experience in making this game.  
           <br></br><br></br>
 
@@ -57,6 +57,35 @@ export default function Home() {
           Project 2: Rodent Reboot
 
         </h3>
+
+        <iframe
+            className="mx-auto"
+            width="500" 
+            height="400"
+            src="https://www.youtube.com/watch?v=Q8Xg16uNTP0"
+            title="Rodent Reboot"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen
+          ></iframe>
+       
+        <h4 className="text-xl py-8">
+      
+          I present to you, Rodent Reboot ☝! 
+          <br></br><br></br>
+          This is a game written in Java using LibGDX using Agile techniques as part of Inf112 coursework.
+          A blog written as a group can be found {" "}
+          <a href = "https://jpiversen.github.io/inf112_group_project/" target="_blank" rel="noopener noreferrer" className="link">here! </a>  <br></br><br></br>
+
+          
+
+          My role in this project was lead tester and developer. As a tester, my responsibilities were to ensure that we complied with minimum coverage (75%) and to carry out manual testing, otherwords play the game a lot! 
+          As a developer, my main responsibilities were code review and writing code. My biggest contribution was the implementation of the item system in which I used a Strategy Design Pattern to randomly spawn and carry out specific powerup effects that the player interacted with at designated item spawn points on the map. 
+
+          <br></br><br></br>
+          For further information, check out the Rodent Reboot {" "}
+          <a href = "https://github.com/KWaloen/Rodent-Reboot" target="_blank" rel="noopener noreferrer" className="link">repository </a>
+          on my GitHub!
+        </h4>
       
       </div>
     
