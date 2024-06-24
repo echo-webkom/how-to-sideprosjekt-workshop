b52a896680ab7b572aebf684335fcf38216dd838
Kai.Waloen <kawal5842@uib.no>
2024-06-24 16:39:02 +0200

    website update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       
         <h1 className="text-3xl font-bold py-8">Hi, my name is Kai 👋</h1>
       
-        <h2 className="text-2xl font-bold py-8">
+        <h2 className="text-2xl py-8">
           
           I am a Computer Science student in Bergen, Norway <br></br><br></br>
           Welcome to my Portfolio Website, also take a look at my {" "}
@@ -17,8 +17,15 @@ export default function Home() {
           <br></br><br></br>
           This site is under constant construction! I am learning as I go! <br></br>
           I am self taught in JavaScript and this page is created using React, Tailwind CSS, and hosted by Vercel. 
+          <br></br>
       
         </h2>
+
+        <h3 className="text-2xl font-bold py-8"> 
+          
+          Project 1: BugShooter
+
+        </h3>
       
           <iframe
             className="mx-auto"
@@ -30,7 +37,7 @@ export default function Home() {
             allowFullScreen
           ></iframe>
        
-        <h3 className="text-xl py-8">
+        <h4 className="text-xl py-8">
       
           I am proud to introduce my first original game, BugShooter ☝. This is a game written in Java using Swing in a period of 4 weeks as part of the Inf101 coursework. <br></br><br></br>
           Growing up, I played a lot of Flash games on Newgrounds.com and my favorite were top down zombie shooters, so I challenged myself to make one. Making this game was a fun, educational, and stressful experience that took a lot of banging my head on the problem but things turned out quite nicely. One day, it occurred to me that instead of a zombie shooter, a bug shooter game would be more appropriate to reflect my experience in making this game.  
@@ -39,7 +46,7 @@ export default function Home() {
           For further information, check out the BugShooter {" "}
           <a href = "https://github.com/KWaloen/BugShooter" target="_blank" rel="noopener noreferrer" className="link">repository </a>
           on my GitHub!
-        </h3>
+        </h4>
       
       </div>
     
