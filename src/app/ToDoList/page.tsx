0e545f7d9c9d1412aefe53b6d5a71950dae50f71
Kai.Waloen <kawal5842@uib.no>
2024-07-14 20:00:37 +0200

    push

--- src/app/ToDoList/page.tsx
+++ src/app/ToDoList/page.tsx
@@ -1,7 +1,6 @@
 //This ToDO List was created based on this tutorial: https://www.youtube.com/watch?v=Rh3tobg7hEof
 
-//use client is necessary for NextJS to not throw a hissy fit
-"use client"
+
 
 import { useEffect, useState } from "react"
 import { NewToDoForm } from "./NewToDoForm"
