35a3c66fcc50b4c3ddec6440839bfd96e64ca261
Kai.Waloen <kawal5842@uib.no>
2024-10-03 13:37:28 +0200

    farmors stuff

---
+++ src/app/ReactProjects/FarmorsStuff/IntroText.tsx
@@ -0,0 +1,12 @@
+
+export default function IntroductionText() {
+    return(
+        <div className = "card">
+        
+            <h1 className = "text">
+                This is a page to distribute Farmor's belongings after she moved to the sykehjem.
+            </h1>
+       
+        </div>
+    )
+}
--- src/app/ReactProjects/FarmorsStuff/page.jsx
+++ src/app/ReactProjects/FarmorsStuff/page.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import { useState } from "react"
+import IntroText from "./IntroText"
+
+import Footer from "../../FrontPage/Footer"
+import Banner from "../../FrontPage/Banner"
+import NavBar from "@/app/FrontPage/NavBar"
+
 export default function FarmorsStuff() {
     return(
-        <div>Farmors Stuff</div>
+        <div className = "page">
+            <NavBar />
+            <Banner message = "Farmors Stuff" />
+            <IntroText />
+                
+            <Footer />
+        </div>
     )
 }
