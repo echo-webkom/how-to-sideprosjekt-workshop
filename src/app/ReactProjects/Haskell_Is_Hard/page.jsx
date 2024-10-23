b98f24427d1c134614c6c6210afbe4dda68b5fff
Kai.Waloen <kawal5842@uib.no>
2024-10-23 20:11:18 +0200

    Set up haskellishard page

---
+++ src/app/ReactProjects/Haskell_Is_Hard/page.jsx
@@ -0,0 +1,12 @@
+import NavBar from "@/app/FrontPage/NavBar"
+
+export default function Haskell_Is_Hard() {
+    return(
+
+        <div className = "page">
+            <NavBar />
+
+        </div>
+
+    );
+}
